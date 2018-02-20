bd9a6b1b7757b77415f6669b54f82195a693f4e1
ikaru <syafrizalizal17@gmail.com>
2018-02-20 13:12:24 +0700

    First Commit
    First Commit

---
+++ src/pages/room/room.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { RoomPage } from './room';
+
+
+@NgModule({
+  declarations: [
+    RoomPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(RoomPage),
+  ],
+})
+export class RoomPageModule {}
